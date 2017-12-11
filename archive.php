de9b19aa0d22a6b8a06ba668a437d11d6eb837e7
Dmitry Bochkov <dmitry.bochkov@gmail.com>
2017-12-11 17:54:17 +0200

    Part 37 finished

--- archive.php
+++ archive.php
@@ -7,14 +7,14 @@ get_header();
 
   <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
-      
+
       <header class="archive-header text-center">
         <?php the_archive_title( '<h1 class="page-title">',  "</h1>" ); ?>
       </header>
 
       <?php if ( is_paged() ): ?>
         <div class="container text-center container-load-previous">
-          <a class="btn-sunset-load sunset-load-more" data-prev="1" data-archive="<?php echo $_SERVER['REQUEST_URI']; ?>" data-page="<?php echo sunset_check_paged(1); ?>" data-url="<?php echo admin_url( 'admin-ajax.php' ); ?>">
+          <a class="btn-sunset-load sunset-load-more" data-prev="1" data-archive="<?php echo sunset_grab_current_uri(); ?>" data-page="<?php echo sunset_check_paged(1); ?>" data-url="<?php echo admin_url( 'admin-ajax.php' ); ?>">
             <span class="sunset-icon sunset-loading"></span>
             <span class="text">Load More</span>
           </a>
@@ -43,7 +43,7 @@ get_header();
       </div> <!-- .container -->
 
       <div class="container text-center">
-        <a class="btn-sunset-load sunset-load-more" data-page="<?php echo sunset_check_paged(1); ?>" data-archive="<?php echo $_SERVER['REQUEST_URI']; ?>" data-url="<?php echo admin_url( 'admin-ajax.php' ); ?>">
+        <a class="btn-sunset-load sunset-load-more" data-page="<?php echo sunset_check_paged(1); ?>" data-archive="<?php echo sunset_grab_current_uri(); ?>" data-url="<?php echo admin_url( 'admin-ajax.php' ); ?>">
           <span class="sunset-icon sunset-loading"></span>
           <span class="text">Load More</span>
         </a>
