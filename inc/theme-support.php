147c80981667c6b250cf9fc0c714e1ce80b5cd82
Dmitry Bochkov <dmitry.bochkov@gmail.com>
2017-12-28 14:37:23 +0200

    Part 64 finished

--- inc/theme-support.php
+++ inc/theme-support.php
@@ -236,3 +236,10 @@ function mailtrap($phpmailer) {
 }
 
 add_action('phpmailer_init', 'mailtrap');
+
+// Initilize global Mobile detect
+function mobileDetectGlobal() {
+  global $detect;
+  $detect = new Mobile_Detect;
+}
+add_action( 'after_setup_theme', 'mobileDetectGlobal' );
